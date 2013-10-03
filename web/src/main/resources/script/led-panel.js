0e2477351b2ad9ad4b6f2e6bf027127de5e3e429
Rikard Wigforss <rikard.wigforss@gmail.com>
2013-10-03 09:58:15 +0200

    KA_JMX-21: Added offset X and Y values to introduce padding, fixed javascript namespace.

--- web/src/main/resources/script/led-panel.js
+++ web/src/main/resources/script/led-panel.js
@@ -1,4 +1,18 @@
-LedPanel = 
+if(typeof org=='undefined') {
+	org = {};
+}
+if(!org.hasOwnProperty("kasource")) {
+	org.kasource = {};
+}
+if(!(org.kasource.hasOwnProperty("jmx"))) {
+	org.kasource.jmx = {};
+} 
+if(!(org.kasource.jmx.hasOwnProperty("widget"))) {
+	org.kasource.jmx.widget = {};
+} 
+
+
+org.kasource.jmx.widget.LedPanel = 
 	function (containerId, options) {
 		this.options = {layout: 'vertical', showLabels: true, color: '#00FF00', labelRotation: 45, data:[]};
 		this.id = containerId;
@@ -22,42 +36,44 @@ LedPanel =
 		this.options.layout = this.options.layout.toLowerCase();
 		
 		var container = document.getElementById(this.id);
-	    var paper = new Raphael(container); 
-	    paper.setViewBox(0, 0, container.offsetWidth, container.offsetHeight, true);
-	   
+	    var paper = new Raphael(this.id); 
+	    var set = paper.set();
 	    var panelWidth = 0;
 		var panelHeight = 0;
-		if(this.options.layout == 'horizontal') {
+		if(this.options.layout == 'vertical') {
 			panelHeight = 10 + this.options.data.length*50; 	
 			panelWidth = 60;
 		} else {
 			panelHeight = 60;
 		  	panelWidth = 10 + this.options.data.length*50; 	
 		}
-		
-		var panel = paper.rect(0, 0, panelWidth, panelHeight); 
+		var offsetX = 5;
+		var offsetY = 5;
+		var panel = paper.rect(offsetX, offsetY, panelWidth, panelHeight); 
 	    panel.glow({
 	        color: '#444',
 	        offsety: 3,
 	        offsetx: 3
 	    });
 	    panel.attr({stroke: '#000', 'stroke-width': 4, gradient: '90-#292929-#4D4D4D'});
-		
+		set.push(panel);
 		for(var i=0; i < this.options.data.length; i++) {
 	    	var ledGlow = paper.set();
 	    	
 	    	var led = {};
 	    	var label;
-	    	if(this.options.layout == 'horizontal') {
-	    		led = paper.circle(30, 30+(i*50), 20);
+	    	if(this.options.layout == 'vertical') {
+	    		led = paper.circle(30 + offsetX, 30+(i*50) + offsetY, 20);
+	    		set.push(led);
 	    		if(this.options.showLabels) {
-	    			label = paper.text(75, 30 + (i*50), options.data[i].title);
+	    			label = paper.text(75 + offsetX, 30 + (i*50) + offsetY, options.data[i].title);
 	    			
 	    		}
 	    	} else {
-	    		led = paper.circle(30+(i*50), 30, 20);
+	    		led = paper.circle(30+(i*50)+offsetX, 30+offsetY, 20);
+	    		set.push(led);
 	    		if(this.options.showLabels) {
-	    			label = paper.text(30 + (i*50), 75, options.data[i].title);
+	    			label = paper.text(30 + (i*50) + offsetX, 75 + offsetY, options.data[i].title);
 	    			
 	    		}
 	    		
@@ -69,7 +85,8 @@ LedPanel =
 	    		
     			label.rotate(this.options.labelRotation,bbox.x,bbox.y);
     			labelBox.rotate(this.options.labelRotation,bbox.x,bbox.y);
-    			
+    			set.push(label);
+    			set.push(labelBox);
     			ledGlow.push(labelBox.glow({color: this.options.color, opacity: 0.3, width: 10}));
 	    	}
 	    	ledGlow.push(led.glow({opacity: 0.7, color: this.options.color, width: 20}));
@@ -85,24 +102,23 @@ LedPanel =
 			this.leds.push({graphic: led, glow: ledGlow, enabled: this.options.data[i].enabled, title: this.options.data[i].title});
 	    }
 		
+		 var viewBox = set.getBBox();
+		 paper.setViewBox(0, 0, viewBox.width + offsetX, viewBox.height + offsetY, true);
+		// paper.canvas.setAttribute('preserveAspectRatio', 'none'); 
+		
 		this.enableLed = function (index, enabled) {
 			var led = this.leds[index];
 			if(led) {
 				if(enabled && !led.enabled) {
-					for(var i=0; i < 8; i++) {
-						var alpha = 0.2+(i*0.1);
-						led.graphic.animate({opacity: alpha}, 100*i, 'bounce');
-					}
+					led.graphic.animate({opacity: 1}, 800, 'bounce');
+					
 					var glow = led.glow;
 					setTimeout(function(){glow.show()},800);
 					led.enabled = true;
 				} else if(!enabled && led.enabled) {
 					led.glow.hide();
-					for(var i=0; i < 8; i++) {
-						var alpha = 1.0-(i*0.1);
-						led.graphic.animate({opacity: alpha}, 100*i, 'bounce');
-			    	
-					}		
+					led.graphic.animate({opacity: 0.2}, 800, 'bounce');
+							
 					led.enabled=false;
 				}
 			}
